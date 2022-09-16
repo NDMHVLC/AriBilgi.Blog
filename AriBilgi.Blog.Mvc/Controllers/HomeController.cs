5630de8da0300a4e9973049ebc8f1505c49f08d8
nedim havlaci <nedimhavlaci@mail.com>
2022-09-16 16:04:24 +0300

    CategoryDto

--- AriBilgi.Blog.Mvc/Controllers/HomeController.cs
+++ AriBilgi.Blog.Mvc/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using AriBilgi.Blog.Data.Abstract;
 using AriBilgi.Blog.Entities.Concrete;
+using AriBilgi.Blog.Mvc.Models.Home;
 using AriBilgi.Blog.Shared.Data.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -24,15 +25,43 @@ namespace AriBilgi.Blog.Mvc.Controllers
         }
         #region
         [HttpPost]
-        public List<User> GetUserList()
+        public List<UserDTO> GetUserList()
         {
-            return _unitOfWork.Users.GetAll();
+            List<User> userList = _unitOfWork.Users.GetAll();
+            List<UserDTO> userDTOList = new List<UserDTO>();
+
+            foreach (User user in userList)
+            {
+                userDTOList.Add(new UserDTO
+                {
+                    Id = user.Id,
+                    NameSurname = user.Name + " " + user.Surname,
+                    ArticleCount = _unitOfWork.Articles.Count(x=>x.UserId==user.Id),
+                    CommentCount = _unitOfWork.Comments.Count(x=>x.UserId==user.Id),
+                    Role=_unitOfWork.UserRoles.Get(x=>x.Id == user.UserRoleId).Title
+                });
+            }
+            userList = null;
+            return userDTOList;
         }
 
         [HttpPost]
-        public List<Category> GetCategoryList()
+        public List<CategoryDTO> GetCategoryList()
         {
-            return _unitOfWork.Categories.GetAll();
+            List<Category> categoryList = _unitOfWork.Categories.GetAll();
+            List<CategoryDTO> categoryDTOList = new List<CategoryDTO>();
+
+            foreach (Category category in categoryList)
+            {
+                categoryDTOList.Add(new CategoryDTO
+                {
+                    Id =category.Id,
+                    Name = category.Title,
+                    ArticleCount = _unitOfWork.Articles.Count(x=>x.CategoryId== category.Id),
+                });
+            };
+            categoryList = null;
+            return categoryDTOList;
         }
         [HttpPost]
         public List<Article> GetArticleList()
